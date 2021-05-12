e4c44edd9f000dafdd24a419afda7cae1435c406
Omar Irfan <omar.irfan18@gmail.com>
2021-05-11 23:47:06 -0400

    added Appointment views to webpack-dev-server environment

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,9 @@ import "components/Application.scss";
 
 import DayList from 'components/DayList'
 
+import 'components/Appointment'
+import Appointment from "components/Appointment";
+
 const days = [
   {
     id: 1,
@@ -22,11 +25,60 @@ const days = [
   },
 ];
 
-
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Omar Irfan",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Taimur Irfan",
+      interviewer: {
+        id: 4,
+        name: "LeBron James",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  }
+];
 
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday')
+
+  const parsedAppointments = appointments && appointments.map (appointment => <Appointment
+  key={appointment.id}
+  {...appointment}/>)
 
   return (
     <main className="layout">
@@ -51,7 +103,8 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
     
@@ -59,3 +112,10 @@ export default function Application(props) {
   );
 }
 
+// const parsedInterviewers = interviewers && interviewers.map (interviewer => <InterviewerListItem 
+//   key={interviewer.id}
+//   id={interviewer.id}
+//   name={interviewer.name}
+//   avatar={interviewer.avatar}
+//   selected={interviewer.id === props.value}
+//   setInterviewer={event => props.onChange(interviewer.id)}/>)
